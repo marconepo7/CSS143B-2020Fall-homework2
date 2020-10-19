220d2fde20f903e286ee7144cea9c2b20994dfc0
Marco <marcon7@uw.edu>
2020-10-19 16:44:55 -0700

    problem 1 wip works until case 4

--- src/main/java/Problem1.java
+++ src/main/java/Problem1.java
@@ -6,5 +6,30 @@ public class Problem1 {
 
     // Do not change signature (function name, parameters type, and return type)
     public static void colorSort(int[] nums) {
+        int p0 = 0;
+        int p2 = nums.length - 1 ;
+
+        for (int i = 0 ; i < nums.length - 1 ; i++){
+            if (nums[i] == 2 && i != p2) {
+                do {
+                    swap(nums, i, p2--);
+                }
+                while (nums[i] == 2);
+            }
+
+            if (nums [i] == 0){
+                if (i != p0) {
+                        swap(nums, i, p0++);
+                }
+                else {
+                    p0++;
+                    }
+                }
+            }
+        }
+    public static void swap(int[] data, int j, int k) {
+        int temp = data[j];
+        data[j] = data[k];
+        data[k] = temp;
     }
 }
